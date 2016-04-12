62755536eaf2aebd521bceafcc7b5d046bc8b33c
Michael Gallegos <mklgallegos@gmail.com>
2016-04-12 14:32:29 -0700

    implement and test fullName computed property on user model

--- app/models/user.js
+++ app/models/user.js
@@ -3,6 +3,11 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   firstName: DS.attr(),
   lastName: DS.attr(),
+  fullName: Ember.computed(function() {
+    let firstName = this.get('firstName'),
+        lastName  = this.get('lastName');
+    return firstName + ' ' + lastName;
+  }),
   admin: DS.attr('boolean', { defaultValue: false}),
   tickets: DS.hasMany('ticket', {async: true}),
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -15,16 +15,11 @@ export default Ember.Route.extend({
       var userId = this.get('currentUser.content.userId');
       var newTicket = this.store.createRecord('ticket', params);
 
-      // grab the user record which matches the session service user
-      // then add newQuestion to the questions property of that user
-      // then save newQuestion
-      // then save user
-
       this.store.findRecord('user', userId).then(function (user) {
         user.get('tickets').addObject(newTicket);
         newTicket.save().then(function() {
-        return user.save();
-      });
+          return user.save();
+        });
       });
     }
   }
