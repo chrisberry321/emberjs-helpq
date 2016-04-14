565d4d8097b06957b1dd59dbad24de372035937f
Michael Gallegos <mklgallegos@gmail.com>
2016-04-14 12:35:45 -0700

    refactor avg-resolution-time helper

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,7 +3,4 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   session: Ember.inject.service('session'),
 
-
-
-
 });
--- app/helpers/avg-resolution-time.js
+++ app/helpers/avg-resolution-time.js
@@ -4,14 +4,22 @@ export function avgResolutionTime(params) {
 
   var allTickets = params[0];
 
+  var allClosedTickets = [];
+
   var timeTotal = 0;
 
-    for (var i = 0; i < allTickets.get('length'); i++) {
-      var dateCreated = allTickets.objectAtContent(i).get('datecreated');
-      var dateClosed = allTickets.objectAtContent(i).get('dateclosed');
-      var duration = dateClosed - dateCreated;
-      timeTotal += duration;
+  for (var i = 0; i < allTickets.get('length'); i++) {
+    if (allTickets.objectAtContent(i).get('closed', true)) {
+      allClosedTickets.push(allTickets.objectAtContent(i));
     }
-    return Math.round(moment.duration(timeTotal / allTickets.get('length')).asMinutes());
+  }
+    
+  for (var i = 0; i < allClosedTickets.get('length'); i++) {
+    var dateCreated = allClosedTickets[i].get('datecreated');
+    var dateClosed = allClosedTickets[i].get('dateclosed');
+    var duration = dateClosed - dateCreated;
+    timeTotal += duration;
+  }
+    return Math.round(moment.duration(timeTotal / allClosedTickets.length).asMinutes());
   }
 export default Ember.Helper.helper(avgResolutionTime);
